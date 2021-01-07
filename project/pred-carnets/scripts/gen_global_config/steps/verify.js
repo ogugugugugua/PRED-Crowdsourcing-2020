e073329d7725121afbe538a040f762a287d0053d
E178298C <maximilien.rioult@etu.univ-nantes.fr>
2021-01-07 01:10:35 +0100

    verify step functional

--- project/pred-carnets/scripts/gen_global_config/steps/verify.js
+++ project/pred-carnets/scripts/gen_global_config/steps/verify.js
@@ -1,13 +1,51 @@
-/*
+const fs = require('fs');
+const { strFormat } = require('./utils/utils');
+const _ = require('lodash');
 
-          "help": {
-        "title": "blabla"
-        "body": "fernguioengzefezfz"
-    },
-
-    `consensus_${subject.name}`
-
-*/
 module.exports = function (config, subjects) {
-  console.log("Verify config generated!");
+  
+  let verifyConfig = _.clone(config);
+  let verifyTasks = {};
+
+  for(let i = 0; i < subjects.length; i++){
+    for(let j = 0; j < subjects[i].categories.length; j++){
+      let suppElts = subjects[i].categories[j].supp_elements;
+      let name = `tr_${subjects[i].name}_${subjects[i].categories[j].name}`
+      verifyTasks[`transcribed_${name}`] = {
+        instruction: config.instructions,
+        tool: "verifyTool",
+        tool_config: {
+          "displays_transcribe_button": true
+        },
+        help: { "file": "" },
+        generates_subject_type: "consensus_" + name
+      }
+
+      for (let k = 0; k < subjects[i].categories[j].supp_elements.length; k++){
+        if (!subjects[i].categories[j].supp_elements[k].toVerify) continue;
+        let speName = name + '_' + subjects[i].categories[j].supp_elements[k]
+        verifyTasks[`transcribed_${speName}`] = {
+            instruction: config.instructions,
+            tool: "verifyTool",
+            tool_config: {
+              "displays_transcribe_button": true
+            },
+            help: { "file": "" },
+            generates_subject_type: "consensus_" + speName
+          }
+      }
+    }
+  }
+    
+  delete verifyConfig['instructions'];
+
+  let globalVerifyConfig = Object.assign(
+    verifyConfig,
+    {
+      tasks : verifyTasks
+    })
+
+  fs.writeFileSync('./output/'+config.name+'.json', JSON.stringify(globalVerifyConfig,null,2));
+
+  console.log("Verify config file generated");
 }
